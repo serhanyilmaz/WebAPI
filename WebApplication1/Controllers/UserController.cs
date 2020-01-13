48aae077501ecfc3170a8d8f512a53c91e459a90
serhanyilmaz <srhnylmz_3458@hotmail.com>
2020-01-13 09:27:33 +0300

    first commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Users> Get()
+        {
+            using (userLogin le =new userLogin())
+            {
+                return le.Users.ToList();
+            }
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
